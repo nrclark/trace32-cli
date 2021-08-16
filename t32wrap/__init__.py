0aab70e817fee82d48683dc4e5b87e43081bb282
Nick Clark <nick.clark@getcruise.com>
2021-08-16 16:57:27 -0700

    Add API wrapper and the start of a top-level program.
    Add the externally-developed t32api wrapper for interacting with
    Trace32 via its TCP/IP C-API. Add the skeleton of a top-level
    program to t32wrap.py. Lots more to come.

--- t32wrap/__init__.py
+++ t32wrap/__init__.py
@@ -1,7 +1,12 @@
 """ Module for wrapping Trace32 into an easier-to-automate interface. """
 
+from . import common
 from .t32run import Podbus
 from .t32run import Trace32Subprocess
+from .t32wrap import main
+from .t32api import Trace32API
 
 del t32run
+del t32wrap
+del t32api
 del common
